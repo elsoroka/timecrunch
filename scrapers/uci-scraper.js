d8fb54acbcaf0011cd5abedf42b2e8a9007da742
Joshua Kuan <jckuan1@uci.edu>
2019-12-18 02:18:23 -0500

    uci js scraper?

---
+++ scrapers/uci-scraper.js
@@ -0,0 +1,44 @@
+const WebSocAPI = require('websoc-api');
+
+options = {
+    term:'2019 Winter',
+    department:'I&C SCI'
+}
+
+const result = WebSocAPI.callWebSocAPI(options);
+finalJson = { "depts" : []};
+result.then((json) => {
+    final_res = { };
+    schools = []
+    json["schools"].forEach(school => {
+
+        depts = [];
+        school["departments"].forEach(department => {
+            courses = [];
+            department["courses"].forEach(course => {
+                var sections = [];
+                course["sections"].forEach(section => {
+                    tmp_section = section["meetings"];
+                    tmp_section["enrolled"] = section["numCurrentlyEnrolled"]["sectionEnrolled"] == "" ? section["numCurrentlyEnrolled"]["totalEnrolled"] : section["numCurrentlyEnrolled"]["sectionEnrolled"];
+
+                    sections.push(tmp_section);
+                });
+
+                let res = {
+                    "courseNumber": course["courseNumber"],
+                    "courseTitle": course["courseTitle"],
+                    "sections": sections
+                };
+                courses.push(res);
+            });
+            depts.push(courses);
+        });
+        let res = { "deptCode" : school["deptCode"],
+                    "schoolName" : school["schoolName"],
+                    "depts": depts};
+        schools.push(res);
+        final_res = {schools};
+    });
+    //console.log(final_res["depts"]);
+    console.log(JSON.stringify(final_res));
+});
