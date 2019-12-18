c70383ba5387d21913307e7e3d2327b271456835
Joshua Kuan <jckuan1@uci.edu>
2019-12-18 17:44:08 -0500

    scraper modifications for 3 second interval loop through all depts and divisions, doesn't work for '/' in filename

--- scrapers/uci-scraper.js
+++ scrapers/uci-scraper.js
@@ -1,45 +1,94 @@
 const WebSocAPI = require('websoc-api');
-
-options = {
-    term:'2019 Winter',
-    department:'I&C SCI'
-}
-
-const result = WebSocAPI.callWebSocAPI(options);
-finalJson = { "depts" : []};
-result.then((json) => {
-    final_res = { };
-    schools = []
-    json["schools"].forEach(school => {
-
-        depts = [];
-        school["departments"].forEach(department => {
-            courses = [];
-            department["courses"].forEach(course => {
-                var sections = [];
-                course["sections"].forEach(section => {
-                    tmp_section = section["meetings"];
-                    tmp_section["enrolled"] = section["numCurrentlyEnrolled"]["sectionEnrolled"] == "" ? section["numCurrentlyEnrolled"]["totalEnrolled"] : section["numCurrentlyEnrolled"]["sectionEnrolled"];
-                    let res = {"enrolled" : section["numCurrentlyEnrolled"]["sectionEnrolled"] == "" ? section["numCurrentlyEnrolled"]["totalEnrolled"] : section["numCurrentlyEnrolled"]["sectionEnrolled"],
-                    "meetings" : section["meetings"]}
-                    sections.push(res);
-                });
-
-                let res = {
-                    "courseNumber": course["courseNumber"],
-                    "courseTitle": course["courseTitle"],
-                    "sections": sections
-                };
-                courses.push(res);
-            });
-            depts.push({"deptCode": department["deptCode"],
-                        "courses": courses});
+const fs = require("fs");
+const path = require("path")
+uciDepts = fs.readFileSync(path.resolve(__dirname, "uci-depts.txt"), "utf-8").split('\n');
+uciDepts.forEach((deptName, deptDelay) => {
+    if (deptName != "") {
+        ["LowerDiv", "UpperDiv", "Graduate"].forEach((division, divDelay) => {
+            options = {
+                term:'2019 Winter',
+                department: deptName,
+                division: division
+            };
+            setTimeout(cb(options), deptDelay*10000+ divDelay*3000);
         });
-        let res = { "schoolName" : school["schoolName"],
-                    "depts": depts};
-        schools.push(res);
-        final_res = {schools};
-    });
-    //console.log(final_res["depts"]);
-    console.log(JSON.stringify(final_res));
+    }
+
 });
+function cb(options) {
+    return () => callWebSoc(options).then(json => {
+        let data = JSON.stringify(json);
+        let div = options["division"].toLowerCase().replace("div", "");
+        let deptName = options["department"].replace(" ", " ");
+
+        var fileName = options["term"] + " " + div + " ";
+        fileName = fileName.toLowerCase().replace(/ /g, "-");
+        fileName += deptName + ".json";
+        console.log(fileName);
+        fs.writeFileSync(path.resolve(__dirname, '..', 'data', 'uci', fileName), data);
+    });
+}
+// callWebSoc(options).then(json => {
+//     let data = JSON.stringify(json);
+//     let div = options["division"].toLowerCase().replace("div", "");
+//     var fileName = options["term"] + " " + div + ".json";
+//     fileName = fileName.toLowerCase().replace(/ /g, "-");
+//     console.log(fileName);
+//     // fs.writeFileSync(fileName, data);
+// });
+//
+// options = {
+//     term:'2019 Winter',
+//     department:'I&C SCI',
+//     division: 'Graduate'
+// };
+// callWebSoc(options).then(json => {
+//     let data = JSON.stringify(json);
+//     let div = options["division"].toLowerCase().replace("div", "");
+//     var fileName = options["term"] + " " + div + ".json";
+//     fileName = fileName.toLowerCase().replace(/ /g, "-");
+//     console.log(fileName);
+//     // fs.writeFileSync(fileName, data);
+// });
+function callWebSoc(options) {
+    return new Promise( resolve => {
+        const result = WebSocAPI.callWebSocAPI(options);
+        finalJson = { "depts" : []};
+        result.then((json) => {
+            final_res = { };
+            schools = []
+            json["schools"].forEach(school => {
+                depts = [];
+                school["departments"].forEach(department => {
+                    courses = [];
+                    department["courses"].forEach(course => {
+                        var sections = [];
+                        course["sections"].forEach(section => {
+                            tmp_section = section["meetings"];
+                            tmp_section["enrolled"] = section["numCurrentlyEnrolled"]["sectionEnrolled"] == "" ? section["numCurrentlyEnrolled"]["totalEnrolled"] : section["numCurrentlyEnrolled"]["sectionEnrolled"];
+                            let res = {"enrolled" : section["numCurrentlyEnrolled"]["sectionEnrolled"] == "" ? section["numCurrentlyEnrolled"]["totalEnrolled"] : section["numCurrentlyEnrolled"]["sectionEnrolled"],
+                            "meetings" : section["meetings"]}
+                            sections.push(res);
+                        });
+
+                        let res = {
+                            "courseNumber": course["courseNumber"],
+                            "courseTitle": course["courseTitle"],
+                            "sections": sections
+                        };
+                        courses.push(res);
+                    });
+                    depts.push({"deptCode": department["deptCode"],
+                                "courses": courses});
+                });
+                let res = { "schoolName" : school["schoolName"],
+                            "depts": depts};
+                schools.push(res);
+                final_res = {schools};
+            });
+            //console.log(final_res["depts"]);
+            //console.log(JSON.stringify(final_res));
+            resolve(final_res);
+        });
+    });
+}
