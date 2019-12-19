fc2b45bfc8ccf28def92031f1150921b45dd779b
Emiko Soroka <esoroka@stanford.edu>
2019-12-19 09:23:21 -0800

    Added a POST handler but I am not sure how to rerender the heatmap. I can rerender crunch_interface but I don't know how to pass parameters in that will change the 0's in the heatmap table. It also console.logs the list of departments.

--- routes/timecromch.js
+++ routes/timecromch.js
@@ -8,6 +8,18 @@ router.get('/', function(req, res, next) {
   //res.send('timecromchh pepega: hello worldjk');
 });
 
+/* POST to handle the submit button ~ELS */
+router.post('/', function(req, res, next) {
+	console.log("POST request with body:")
+	console.log(req.body)
+	// Put the DB query here
+	for (const dept_name of req.body.dept_names) {
+		// Do some THING
+		console.log(dept_name)
+	}
+	res.render('crunch_interface', {title: 'timecrunch'})
+})
+
 //  Display interface on GET.
 exports.course_create_get = function(req, res, next) {
   res.render('crunch_interface', {title: 'timecrunch'})
