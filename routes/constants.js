523fc31916e0d0ab9373c637d7d14d4c984bab33
Emiko Soroka <esoroka@stanford.edu>
2019-12-19 19:51:04 -0800

    Added passing data array as a parameter into layout and then into heatmap_view. Able to populate heatmap with data if data is not undefined (otherwise default to populating with zeroes). Populating heatmap_view with random numbers as a test.

---
+++ routes/constants.js
@@ -0,0 +1,5 @@
+// attention! emi created this file and she is a C programmer
+// so if this the wrong way to do it, please fix. Thanks.
+
+const N_HEATMAP_ROWS = 14;
+const N_HEATMAP_COLS =  5;
--- routes/timecromch.js
+++ routes/timecromch.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET timecronch app  */
 
 router.get('/', function(req, res, next) {
-  res.render('layout', {title: 'timecrunch'})
+  res.render('layout', {title: 'timecrunch', data:undefined})
   //res.send('timecromchh pepega: hello worldjk');
 });
 
@@ -13,16 +13,22 @@ router.post('/', function(req, res, next) {
 	console.log("POST request with body:")
 	console.log(req.body)
 	// Put the DB query here
-	for (const dept_name of req.body.dept_names) {
-		// Do some THING
-		console.log(dept_name)
+	// Maybe the DB query function should return the 2D array representing the heatmap
+	// 14 hours * 6 10-minute increments = 14*6 rows
+	const heatmap = new Array(14*6).fill(0).map(() => new Array(5).fill(0));
+
+	// Fill with some fake data
+	for (let i=0; i<heatmap.length; ++i) {
+		for (let j=0; j<heatmap[i].length; ++j) {
+			heatmap[i][j] = Math.floor((Math.random() * 100) + 1); // Random fake data, 0 - 100
+		}
 	}
-	res.render('layout', {title: 'timecrunch'})
+	res.render('layout', {title:'timecrunch', data:heatmap})
 })
 
 //  Display interface on GET.
 exports.course_create_get = function(req, res, next) {
-  res.render('crunch_interface', {title: 'timecrunch'})
+  res.render('layout', {title: 'timecrunch', data:undefined})
 };
 
 module.exports = router;
