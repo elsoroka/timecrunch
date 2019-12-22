a5162d7dbb280ebca255845cdf247951b60ad2f3
Emiko Soroka <esoroka@stanford.edu>
2019-12-22 14:17:52 -0800

    Added functions to fill in the heatmap array from course objects described in course.js. Added some fake course data in interfaceController to test populating the heatmap. Confirmed it fills the table. A lot of bad assumptions were made regarding classes starting no earlier than 8AM.

--- controllers/interfaceController.js
+++ controllers/interfaceController.js
@@ -39,6 +39,32 @@ exports.exec_query = [
 				return next(err);
             if (courses)
             {
+                console.log("COURSES:");
+                console.log(courses);
+                // FAKE DATA, REMOVE
+                courses = [
+                {"timeisTBA":false,
+                 "startTime":8*60,
+                 "endTime"  :8*60+50,
+                 "days"     :[0,2,4],
+                 "enrolled" :100},
+                {"timeisTBA":true,
+                 "startTime":9*60,
+                 "endTime"  :9*60+50,
+                 "days"     :[0,2,4],
+                 "enrolled" :200},
+                {"timeisTBA":false,
+                 "startTime":12*60+30,
+                 "endTime"  :13*60+50,
+                 "days"     :[1,3],
+                 "enrolled" :250},
+                 {"timeisTBA":false,
+                 "startTime":13*60,
+                 "endTime"  :14*60+50,
+                 "days"     :[3],
+                 "enrolled" :50},
+                ];
+
 				let heatmap_data  = generate_heatmap(courses); //2d array populated with the course info
                 /*
                           0   1   2   3   4
@@ -54,8 +80,10 @@ exports.exec_query = [
                  { heatmap: [[]] } //heatmap is an array of arrays like above ^
                  */
                 let heatmap_dict = heatmap_data //view_dict(heatmap_data);
-				res.render('timecrunch_interface_with_heatmap_data', heatmap_dict);
-				return;
+				// ELS: Not sure what this is so I temporarily added the old render call
+                // res.render('timecrunch_interface_with_heatmap_data', heatmap_dict);
+				res.render('layout', {title: 'timecrunch', data:heatmap_data});
+                return;
             }
 			res.render('timecrunch_interface_no_data', no_data);
             return;
@@ -67,11 +95,46 @@ exports.exec_query = [
 function generate_heatmap(courses)
 {
 
-	const heatmap = new Array(14*6).fill(0).map(() => new Array(5).fill(0));
-    for (course in courses)
-    {
-        // TODO: use info to accumulate 2D histogram packaged in JSON so we can pass the object to pug 
-        // accumulate_heatmap(course_doc, hmap);
+    let hm = new Array(14*6).fill(0).map(() => new Array(5).fill(0));
+    for (let course of courses) {
+        //hm[ROW IS DETERMINED BY TIME][COL IS DETERMINED BY DAY] += course.enrolled
+        //maybe like:
+        if (course.timeIsTBA) {
+            continue;
+        }
+
+        let rows = getRows(course.startTime, course.endTime);
+        let cols = course.days;
+        console.log("Rows, cols", rows, cols);
+        for (let row of rows) {
+            for (let col of cols) {
+                hm[row][col] += course.enrolled;
+            }
+        }
     }
-    return heatmap;
+    return hm;
+}
+
+function getRows(startMinutes, endMinutes)
+{
+    // ASSUMPTION: The table starts at 8am and goes by 10 minute increments
+    // TBD: Something about earlier/later times here
+    // Move startMinutes and endMinutes to start at 8am
+    startMinutes -= 8*60;
+    endMinutes   -= 8*60;
+    if (startMinutes < 0 || endMinutes < 0) { // YIKES
+        console.log("Something badly wrong: class before 8AM");
+        return []; // safe failure mode, kind of
+    }
+    /* Downsample from minutes to 10-minute increments with conservative rounding.
+    This means if you have a startTime 12:09 it will map to 12:00
+    an endTime 12:01 will map to 12:10
+    and a time length from 12:09 to 1:01 will be 70 minutes (12:00 - 1:10).*/
+    let start = Math.floor(startMinutes/10);
+    let length = Math.ceil((endMinutes/10)-start);
+    
+    rows = Array(length).fill().map((_, i) => i+start);
+    //console.log("For start and end:", startMinutes, endMinutes);
+    //console.log("Rows:", rows);
+    return rows;
 }
