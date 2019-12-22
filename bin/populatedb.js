93a87998ae43819e630659f46af98eae92248772
Magks <magks@github.com>
2019-12-22 13:57:42 -0800

    bin/populatedb.js - working on uci time

--- bin/populatedb.js
+++ bin/populatedb.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const fs = require('fs')
 const async = require('async')
+const Course = require('../models/course')
 let universities = []
 let courses = []
 const userArgs = process.argv.slice(2);
@@ -13,7 +14,6 @@ if (!userArgs[0].startsWith('mongodb')) {
     return -1;
 }
 const async = require('async')
-const Course = require('../models/course')
 const mongoose = require('mongoose');
 let mongoDB = userArgs[0];
 mongoose.connect(mongoDB, { useNewUrlParser: true });
@@ -141,23 +141,26 @@ functions.behavior1();
 */
 
 
-function courseCreate(uni, div, dept, title, number, sections, cb) {
-    courseinfo = {
+function courseCreate(uni, div, dept, number, title, sections, cb) {
+    let courseinfo = {
         university: uni, 
         division: div, 
         department: dept, 
-        courseTitle: title, 
         courseNumber: number,
-        sections: sections
+        courseTitle: title, 
     };
+    //console.log(sections);
+    // create array of  parsed sections 
+    courseinfo.sections = Course.parseSections(sections);
     let course = new Course(courseinfo);
+    console.log(course.sections[0].meetings);
     course.save( function (err) {
         if (err) {
             cb(err, null);
             return;
         }
         console.log('New Course: ' + course);
-        courses.push(course);
+        //courses.push(course);
         cb(null, course)
     });
 }
@@ -174,31 +177,27 @@ async function ls(path, cb) {
             }
             else {
                 let uni_json = JSON.parse(data);
-                let uni_name = dirent.name.substr(0, dirent.name.lastIndexOf('.'));
+                let uni_name = dirent.name.substr(0, dirent.name.lastIndexOf('.')); // TODO: we should store the name in the file instead to cleanly handle special chars
                 let div, dept, cls, courseNumber, courseTitle, section
                 console.log(uni_name);
                 for (div in uni_json) {
-                    console.log(div)
+                    //console.log(div)
                     for (dept in uni_json[div]) {
-                        console.log(dept)
+                        //console.log(dept)
                         for (course_index in uni_json[div][dept]){
                             cls = uni_json[div][dept][course_index]
-                            console.log(cls)
+                            //console.log(cls)
                             courseNumber = cls.courseNumber
                             courseTitle = cls.courseTitle
-                            courseCreate(uni_name, dept, 
-                            for (section_index in cls.sections)
-                                section = cls.sections[section_index]
-                                console.log(section)
+                            courseCreate(uni_name, div, dept, courseNumber, courseTitle, cls.sections, cb);
                         }
-
                     }
                 }
                 //console.log(uni_json);
                 //console.log(uni_json.lower['STATS'][1]['sections'][0]); //enrolled / meetings[]
                 //courseCreate(uni_name, 
-                universities.push(uni_json);
-                cb(null, uni_json);
+                //universities.push(uni_json);
+                //cb(null, uni_json);
             }
         });
     }
--- models/course.js
+++ models/course.js
@@ -3,14 +3,13 @@ const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema(
 {
-    _id: Schema.Types.ObjectId,
     university: String,
     division: String,  
     department: String,
     // The names of the fields follow what WebSoc and websoc-api calls these
 	courseNumber: String, // e.g. "20A"
 	courseTitle: String,
-	// array of secctions with nested array of meetings
+	// array of sections with nested array of parsed meetings
 	sections: 
     [{
         enrolled: Number,       // enrolled under this department
@@ -23,6 +22,75 @@ const CourseSchema = new Schema(
             days: [Number]
         }]
     }]
-});
+}, {timestamps:{}}); // option timestamps automatically adds createdAt and updatedAt fields
+
+
+//' XX:XX- XX:XXp' to [startMins, endMins] starting from midnight
+function minutesSinceMidnight(unformatted_time) {
+    const parse_hr_min = (hr_min_str) => hr_min_str.split(':').map(t => parseInt(t,10));
+    const calculate_mins_mod12 = (h, m) => (h % 12) * 60 + m;
+    const time_in_minutes = (s, ampm, tag) => {
+        const [hr,min] = parse_hr_min(s) // input: "XX:XX" output: XX, XX as integers
+        let mins_before_noon = 0
+        if (ampm == 'p') // if pm, the end hour always is after 12pm, but the start hour needs to checked explicitly
+            if (tag == "end" || tag == "start" && hr <= 8 )
+                mins_before_noon = 12*60; // greedy: we know this many mins are guaranteed
+        return mins_before_noon + calculate_mins_mod12(hr, min); // add the rest
+    };
+    const [start,end] = unformatted_time.trim().split('-');
+    const ampm = end.includes('p') ? 'p' : 'a'; 
+    let startMins = time_in_minutes(start, ampm, "start");
+    let endMins = time_in_minutes(end, ampm, "end");
+    return [startMins, endMins]
+};
+
+// TODO: validate this in case some university uses a daystring with unexpected chars
+function convertDaysToIntArray(days_string) {
+    const days_re = /[MWF]|Tu|Th|Sa|Su/g;
+    const dayDict = {"M": 0, "Tu": 1, "W": 2, "Th": 3, "F": 4, "Sa": 5, "Su": 6}
+    let dayInts = [] 
+    let match;
+    do {
+        match = days_re.exec(days_string);
+        if (match) {
+            //console.log(match)
+            dayInts.push(dayDict[match[0]]);
+        }
+    } while(match);
+    return dayInts
+}
+
+CourseSchema.statics.parseSections = function(sections) {
+    let parsed_sections = new Array(sections.length)
+    let parsed_section, parsed_meeting
+    // create parsed sections
+    sections.forEach(section => {
+        parsed_section = {}
+        console.log(section);
+        parsed_section.enrolled = section.enrolled;
+        parsed_section.meetings = [];
+        section.meetings.forEach(meeting => {
+            parsed_meeting = {}
+            console.log(meeting)
+            parsed_meeting.bldg = meeting.bldg
+            if (meeting.time == 'TBA' || meeting.days == '') {
+                parsed_meeting.timeIsTBA = true
+                parsed_meeting.startTime = 0
+                parsed_meeting.endTime = 0
+                parsed_meeting.days = [0]
+            }
+            else {
+                let [startMins, endMins] = minutesSinceMidnight(meeting.time)
+                parsed_meeting.startTime = startMins
+                parsed_meeting.endTime = endMins
+                parsed_meeting.days = convertDaysToIntArray(meeting.days)
+                parsed_meeting.timeIsTBA = false
+            }
+            parsed_section.meetings.push(parsed_meeting);
+        });
+        parsed_sections.push(parsed_section);
+    });
+    return parsed_sections;
+};
 
 module.exports = mongoose.model('Course', CourseSchema);
