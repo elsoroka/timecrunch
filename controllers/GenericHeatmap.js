9d3c9b12171f2db8e6c954b4195333f7209f0e6a
Magks <magks@github.com>
2019-12-23 13:44:52 -0800

    removed fake data, we now actually query the database

--- controllers/GenericHeatmap.js
+++ controllers/GenericHeatmap.js
@@ -14,6 +14,18 @@ class GenericHeatmap {
 		        defaultDays      = 5,
 		        timeFormat       = '12',
 		        ) {
+        this.class_id = "GenericHeatmap::";
+        let get_id = (s) => this.class_id + s;
+        this.getRows_id = get_id("getRows:");
+        this.constructor_id = get_id("constructor:");
+        this.dbg = funct_id => {
+            return (variable) => {
+                let varName = Object.keys(variable)[0];
+                let varValue = variable[varName];
+                console.log(`${funct_id} ${varName} = ${varValue}`);
+            };
+        };
+
 		// Data integrity checks
 		// timeStep must be a positive integer and divide evenly into 60 minutes (1 hour)
 		assert(timeStep > 0 && Number.isInteger(60/timeStep));
@@ -25,6 +37,7 @@ class GenericHeatmap {
 		assert(defaultDays >= 5 && defaultDays <= 7);
 		// Are there other time formats? Rewrite when humanity reaches Mars.
 		assert(timeFormat == '12' || timeFormat == '24');
+        console.log(this.constructor_id + "Data integrity checks complete");
 
 		// Data integrity checks complete.
 		this.timeStep   = timeStep;
@@ -37,34 +50,44 @@ class GenericHeatmap {
 		// the array size will change.
 		const nRows = Math.ceil((defaultTimeStop-defaultTimeStart)/timeStep);
 		this.heatmap = new Array(nRows).fill(0).map( () => new Array(defaultDays).fill(0));
+        console.log(this.constructor_id + "Constructor Finished");
 	}
 
 	fill(courses) {
-		for (const course of courses) {
-			if (course.timeIsTBA) {
-            	continue;
-        	}
+        courses.forEach(course => { 
+            this.dbg({course});
+            course.sections.forEach(section => {
+                section.meetings.forEach(meeting => { 
 
-        	const rows = this.getRows(course.startTime, course.endTime);
-        	const cols = this.getCols(course.days);
-        	console.log("Rows", rows, "Cols", cols);
-        	for (const row of rows) {
-        		console.log("row", row);
-	            for (const col of cols) {
-	            	console.log("col", col);
-                	this.heatmap[row][col] += course.enrolled;
-            	}
-        	}
-		}
+                    if (meeting.timeIsTBA) 
+                        return; // in a .forEach, this works like continue, i.e. goes to next element
+                    const rows = this.getRows(meeting.startTime, meeting.endTime);
+                    const cols = this.getCols(meeting.days);
+                    console.log("Rows", rows, "Cols", cols);
+                    rows.forEach(row => { 
+                        console.log("row", row);
+                        cols.forEach( col => {
+                            console.log("col", col);
+                            this.heatmap[row][col] += section.enrolled;
+                        }); //end for cols
+                    }); // end for rows
+
+                }); //end for meetings
+            }); // end for sections
+        }); //end for courses
 	}
 
 	getRows(startMinutes, endMinutes) {
+        let dbg = this.dbg(this.getRows_id);
+        dbg({startMinutes});
+        dbg({endMinutes});
 	    // If startMinutes is EARLIER than the first row, prepend row(s) to display it.
 	    if (startMinutes < this.timeStart) {
 	    	// Guarantee timeStart is a whole hour value
 	    	// Otherwise, it will be difficult to guarantee rows are labeled correctly.
 	    	const hoursToAdd = Math.ceil((this.timeStart - startMinutes)/60);
 	    	// Because we have guaranteed 60/this.timeStep is integer, this is safe.
+            //console.log(`${this.getRows_id} hoursToAdd = ${hoursToAdd}`);
 	    	const newRows = new Array(hoursToAdd*60/this.timeStep).fill(0).map( () => new Array(this.days).fill(0));
 	    	
 	    	this.heatmap.unshift(...newRows);
@@ -86,11 +109,15 @@ class GenericHeatmap {
 	    This means if you have a startTime 12:09 it will map to 12:00
 	    an endTime 12:01 will map to 12:10
 	    and a time length from 12:09 to 1:01 will be 70 minutes (12:00 - 1:10).*/
+        
 	    const start  = Math.floor((startMinutes-this.timeStart)/this.timeStep);
 	    const end    = Math.ceil((endMinutes-this.timeStart)/this.timeStep);
 	    const length = end-start;
+        dbg({end});
+        dbg({start});
+        dbg({length});
 	    
-	    const rows = Array(length).fill().map((_, i) => i+start );
+	    const rows = new Array(length).fill().map((_, i) => i+start );
 	    //console.log("For start and end:", startMinutes, endMinutes);
 	    //console.log("Rows:", rows);
 	    return rows;
@@ -126,7 +153,7 @@ class GenericHeatmap {
 	// Return a list of the days
 	getDayLabels() {
 		const alldays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-		return alldays.slice(0,this.days);
+		return alldays.slice(0,this.days); 
 	}
 }
 
