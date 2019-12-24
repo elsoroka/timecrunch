97aa4494b95bf5fbe36309068b9ecb4c2d1c13e6
Magks <magks@github.com>
2019-12-24 13:13:28 -0800

    commented and re-organized heatmap_d3.js

--- public/scripts/heatmap_d3.js
+++ public/scripts/heatmap_d3.js
@@ -1,42 +1,43 @@
 // be informed by the server data
-/*
-mixin 10minutes(name, index)
-  // Construct a row. If data exists (not undefined), index into it and use the numbers there.
-  tr
-    td.hour= name
-    // See if we have data passed in from layout
-    - if ((undefined == data) || (index >= data.length)) 
-      -{ row1 = [0,0,0,0,0]; }
-    - else { console.log(data[index]); row1 = data[index] }
-
-    each val in row1
-      td.people= val
-*/
-if (server_data.init == "init") {
+if (server_data.init === "init") {
     console.log("wow passed data here...");
 };
 
-date = new Date();
-
-weekdayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var dateString = weekdayNames[date.getDay()] + " " 
-    + date.getHours() + ":" + ("00" + date.getMinutes()).slice(-2) + ":" + ("00" + date.getSeconds()).slice(-2) + ":" + date.getMilliseconds() + " " 
-    + date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear();
-
+/*
+ * Emit a timestamp in web console
+ */
+// TODO: make this a reusable function and arrange so that both server and client can use it
+let days_ts = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let months_ts = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    // Day HR:MIN:SS:MS Month DayNum Year
+let getFormattedTimeStamp = (timestamp) => 
+    days_ts[timestamp.getDay()] + " " 
+    + timestamp.getHours() + ":" 
+    + ("00" + timestamp.getMinutes()).slice(-2) + ":" 
+    + ("00" + timestamp.getSeconds()).slice(-2) + ":" 
+    + timestamp.getMilliseconds() + " " 
+    + months_ts[timestamp.getMonth()] + " " 
+    + timestamp.getDate() + " " 
+    + timestamp.getFullYear();
 //console.log(`[${getFormattedTimeStamp()}] Initializing heatmap...`); 
-
+console.log(`[${getFormattedTimeStamp(new Date)}] Initializing heatmap...`);
+console.log(JSON.stringify(hm));
+console.log(weekdayNames_cus);
+console.log(timeIncrements);
 
 // TODO: programmatically determine width and height 
 // set the dimensions and margins of the graph
-var margin = {top: 100, right: 25, bottom: 30, left: 70},
-  width = Math.max(Math.min(window.innerWidth, 950),500) - margin.left - margin.right,
-  height = Math.max(Math.min(window.innerWidth,850),500)  - margin.top - margin.bottom;
+const margin = {top: 100, right: 25, bottom: 30, left: 70}
+let width = Math.max(Math.min(window.innerWidth, 950),500) - margin.left - margin.right;
+let height = Math.max(Math.min(window.innerWidth,850),500)  - margin.top - margin.bottom;
 
 
-console.log(JSON.stringify(hm));
+/*
+ * Heatmap Dimensions and Placement
+ */
+
 // append the svg object to the body of the page
-var svg = d3.select("#heatmap")
+let svg = d3.select("#heatmap")
 .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -44,43 +45,33 @@ var svg = d3.select("#heatmap")
   .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-// Read the data
-console.log(weekdayNames_cus);
-console.log(timeIncrements);
+/*
+ * Title
+*/
 
-// Build X scales and axis:
-var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(Array.from({length:5},(_,i)=>i))//weekdayNames_cus)
-    .padding(0.05);
+// Add title to graph
+svg.append("text")
+    .attr("x", 0)
+    .attr("y", -60)
+    .attr("text-anchor", "left")
+    .style("font-size", "22px")
+    .text("timecrunch");
 
-console.log(JSON.stringify(timeIncrements));
-svg.append("g")
-    .attr("id", "day-axis")
-    .classed("x axis", true)
-    //.select(".domain").remove()
+// Add subtitle to graph
+svg.append("text")
+    .attr("x", 0)
+    .attr("y", -40)
+    .attr("text-anchor", "left")
+    .style("font-size", "14px")
+    .style("fill", "grey")
+    .style("max-width", 400)
+    .text("when are students in class?")
 
-// Build Y scales and axis:
-var y = d3.scaleBand()
-    .range([ 0, height ])
-    .domain(Array.from({length:84},(_,i)=>i))  //timeIncrements
-    .padding(0.01);
-
-svg.append("g")
-    .attr("id", "time-axis")
-    .style("font-size", 15)
-    .attr("transform", "translate(0," + 0 + ")")
-    .classed("y axis", true)
-
-// Build color scale
-var myColor = d3.scaleLinear()
-    //.interpolator(d3.interpolateInferno)
-    .domain([0, Math.max(...[].concat(...hm))/2])
-    .range(["#FFFFDD", "#3E9583", "#1F2D86"]);
-        //d3.max(hm, function(d) {return d.count; })/2, d3.max(hm, function(d) {return d.count;})])
-
+/*
+ * Tooltip Mouse Hover Info
+ */
 // create a tooltip
-var tooltip = d3.select("#heatmap")
+let tooltip = d3.select("#heatmap")
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip")
@@ -91,26 +82,70 @@ var tooltip = d3.select("#heatmap")
     .style("padding", "5px")
 
 // Three function that change the tooltip when user hover / move / leave a cell
-var mouseover = function(d) {
-tooltip
-  .style("opacity", 1)
-d3.select(this)
-  .style("stroke", "black")
-  .style("opacity", 1)
+let mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)
 }
-var mousemove = function(d) {
-tooltip
-  .html("The exact value of<br>this cell is: " + d.value)
-  .style("left", (d3.mouse(this)[0]+70) + "px")
-  .style("top", (d3.mouse(this)[1]) + "px")
+
+let mousemove = function(d) {
+    tooltip
+      .html("The exact value of<br>this cell is: " + d.value)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
 }
-var mouseleave = function(d) {
-tooltip
-  .style("opacity", 0)
-d3.select(this)
-  .style("stroke", "none")
-  .style("opacity", 0.8)
+
+let mouseleave = function(d) {
+    tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
 }
+
+/*
+ * Axis styles
+ */
+
+// Prepare the group elements for X and Y axes in the svg
+svg.append("g")
+    .attr("id", "day-axis")
+    .style("font-size", 15)
+    .attr("transform", "translate(0," + 0 + ")")
+    .classed("x axis", true)
+    //.select(".domain").remove()
+
+svg.append("g")
+    .attr("id", "time-axis")
+    .style("font-size", 15)
+    .attr("transform", "translate(0," + -25 + ")")
+    .classed("y axis", true)
+
+/*
+ * Temporary Index-Based X and Y scales 
+ *  Use numbers as the domains to more easily access a cell when populating the table.
+ *  These scales get replaced by string labels for the domain after the table is built.
+ */
+// Build X and Y scales
+let x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(Array.from({length:5},(_,i)=>i))//weekdayNames_cus)
+
+let y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(Array.from({length:84},(_,i)=>i))  //timeIncrements
+
+/*
+ * Heatmap Colorscheme
+ */
+// Build color scale
+let myColor = d3.scaleLinear()
+    //.interpolator(d3.interpolateInferno)
+    .domain([0, Math.max(...[].concat(...hm))/2])
+    .range(["#FFFFDD", "#3E9583", "#1F2D86"]);
+        //d3.max(hm, function(d) {return d.count; })/2, d3.max(hm, function(d) {return d.count;})])
 
 // add the squares
 let allArr = svg.selectAll()
@@ -147,51 +182,28 @@ let allArr = svg.selectAll()
 .on("mousemove", mousemove)
 .on("mouseleave", mouseleave)
 
-// Build X scales and axis:
+// Replace X and Y Scales with string labels
 x = d3.scaleBand()
     .range([ 0, width ])
     .domain(weekdayNames_cus)//Array.from({length:5},(_,i)=>i))//weekdayNames_cus)
     .padding(0.05);
 
-svg.select("#day-axis")
-    .style("font-size", 15)
-    .attr("transform", "translate(0," + -2 + ")")
-    .call(d3.axisTop(x).tickSize(0))
-    .select(".domain").remove()
-
-// Build Y scales and axis:
-//timeIncrements.push('10:00p');
 y = d3.scaleBand()
     .range([ 0, height ])
     .domain(timeIncrements)//Array.from({length:84}, (_,x)=>x+'8:00a'))//timeIncrements)//Array.from({length:84},(_,i)=>i))  //timeIncrements
     .padding(-1.11);
 
+
+// Render X and Y axis
+svg.select("#day-axis")
+    .call(d3.axisTop(x).tickSize(0))
+    .select(".domain").remove()
+
 svg.select("#time-axis")
-    .style("font-size", 15)
-    .attr("transform", "translate(10," + -10 + ")")
     .call(
         d3.axisLeft(y)
         .tickSize(0)
     )
     .select(".domain").remove()
 
-/*
-*/
 
-// Add title to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -60)
-    .attr("text-anchor", "left")
-    .style("font-size", "22px")
-    .text("timecrunch");
-
-// Add subtitle to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -40)
-    .attr("text-anchor", "left")
-    .style("font-size", "14px")
-    .style("fill", "grey")
-    .style("max-width", 400)
-    .text("when are students in class?")
