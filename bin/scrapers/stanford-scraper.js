0a698fee762ccb81087cf2e2c83a7d718890137e
Emiko Soroka <esoroka@stanford.edu>
2019-12-27 21:09:53 -0800

    Able to scrape course code and name and text string containing rest of data. Will work on text parser tomorrow / later tonight. Discussed with Max and we will try to access the Stanford course API when school starts but work on this for now because the API is restricted to Stanford affiliates and that could cause a problem later.

--- bin/scrapers/stanford-scraper.js
+++ bin/scrapers/stanford-scraper.js
@@ -26,21 +26,21 @@ let getData = html => {
 		const courseNumber = $(".courseNumber", courseInfo).text();
 		const courseTitle = $(".courseTitle", courseInfo).text();
 		// Retrieve schedule data for one term
-		const meetings = [];
+		const sections = [];
+		// If there is more than one section, there is more than one li sectionDetails.
 		const sections = $('.sectionDetails', course).each(function(i, meeting) {
 			console.log("MEETING", $(this).text());
+			const location = $('a', this).text();
+			sections.push({
+
+			})
 		});
-		console.log("number", courseNumber, "title", courseTitle);
-		//courses.push(retrieveDataFromSearchResult(course));
+		courses.push({
+			division:"Graduate",
+			department:"AA",
+			courseNumber:courseNumber,
+			courseTitle:courseTitle,
+			sections:sections,
+		});
 	});
 }
-
-function retrieveDataFromSearchResult(searchResult) {
-	let courseInfo = searchResult('.courseResult');
-	console.log("\nList:", courseInfo);
-	courseInfo = courseInfo[0];
-	console.log("\nFirst:", courseInfo);
-	const courseNumber = courseInfo('.courseNumber');
-	console.log("At course:", courseNumber);
-}
-
