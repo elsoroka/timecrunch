396fb326cf41e8e52ea144637d8a2be5ae041865
Emiko Soroka <esoroka@stanford.edu>
2019-12-30 13:05:07 -0800

    Fixed the literal spaces in "Students enrolled" and quarter start/end date regex to match any whitespace because in some real data, a tab or other whitespace appeared. Removed instructor names from test data because I don't want them to show up in the repo; that's yikes.

--- bin/scrapers/stanford-scraper.js
+++ bin/scrapers/stanford-scraper.js
@@ -2,12 +2,14 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+testParser();
 
 // Example URL
 // https://explorecourses.stanford.edu/print?filter-term-Winter=on&filter-catalognumber-AA=on&filter-academiclevel-GR=on&filter-coursestatus-Active=on&filter-catalognumber-AA=on&q=AA&descriptions=on&schedules=on
-testParser();
-/*const url = "https://explorecourses.stanford.edu/print?filter-term-Winter=on&filter-catalognumber-AA=on&filter-academiclevel-GR=on&filter-coursestatus-Active=on&filter-catalognumber-AA=on&q=AA&descriptions=on&schedules=on"
+//const url = "https://explorecourses.stanford.edu/print?filter-term-Winter=on&filter-catalognumber-AA=on&filter-academiclevel-GR=on&filter-coursestatus-Active=on&filter-catalognumber-AA=on&q=AA&descriptions=on&schedules=on"
+// Small testcase URL, 3 results
 
+const url = "https://explorecourses.stanford.edu/search?view=catalog&filter-coursestatus-Active=on&page=0&catalog=&academicYear=&q=CS229&collapse=%2C6%2C7%2C"
 axios.get(url).then(response => {
 	//console.log(response);
 	getData(response.data);
@@ -26,29 +28,45 @@ let getData = html => {
 		const courseNumber = $(".courseNumber", courseInfo).text();
 		const courseTitle = $(".courseTitle", courseInfo).text();
 		// Retrieve schedule data for one term
-		const sections = [];
 		// If there is more than one section, there is more than one li sectionDetails.
-		const sections = $('.sectionDetails', course).each(function(i, meeting) {
-			console.log("MEETING", $(this).text());
+		let sections = [];
+		$('.sectionDetails', course).each(function(i, meeting) {
+			//console.log("\nRaw source\n", $(this).text());
 			const location = $('a', this).text();
-			sections.push({
-				enrolled:0,
-				meetings:[{
-					bldg:location,
-					timeIsTBA:false,
-					
-				}]
+			console.log("Location", location);
+			let section = parseDescriptionString($(this).text());
+			if (null != section) {
+				// Fix the building listed in meetings
+				section.meetings.map( (meeting, _) => meeting.bldg = location );
+				sections.push(section);
+			}
+		});
+		// Something went wrong
+		if ([] == sections) {
+			console.log("WARNING: No valid sections found. Skipping:", courseTitle);
+		}
+		else {
+			console.log(courseNumber, courseTitle);
+			sections.map( function(section, _) {
+				console.log("Enrolled:", section.enrolled);
+				section.meetings.map( function(meeting, _) {
+					console.log("\t MEETING", meeting); });
 			});
-		});
-		courses.push({
-			division:"Graduate",
-			department:"AA",
-			courseNumber:courseNumber,
-			courseTitle:courseTitle,
-			sections:sections,
-		});
+			courses.push({
+				division:"Graduate",
+				department:"AA",
+				courseNumber:courseNumber,
+				courseTitle:courseTitle,
+				sections:sections,
+			});
+		}
 	});
-}*/
+	return courses;
+}
+
+
+/* PARSING CODE ONLY BEYOND THIS POINT
+*/
 
 /* Handles a string like
 CEE 107A | 3-5 units | UG Reqs: GER:DB-EngrAppSci, WAY-SI | Class # 7814 | Section 01 |
@@ -59,26 +77,32 @@ function parseDescriptionString(descriptionString) {
 	// Split by "|"
 	substrings = descriptionString.split('|').map((item, _) =>
 		item.trim()); // Remove stray whitespace
-	console.log("Trimmed:", substrings);
 	/* Assumption:
-	 * The first 2 substrings are course name and unit count.
+	 * The first substring is course name
 	 * We already know the name and don't care about the count.
 	 * The class #, section #, and grading option must appear (3 more substrings)
-	 * Other sections such as UG Reqs may be interspersed, but this is not guaranteed.
-	 * We can safely skip at least 5 substrings and scan for the course type (LEC, DIS, LAB...)
+	 * Other sections such as unit count UG Reqs may be interspersed, but this is not guaranteed.
+	 * We can safely skip at least 4 substrings and scan for the course type (LEC, DIS, LAB...)
 	 */
-	let index = 5;
-	// Find the course type
+	let index = 4;
+	// Find the course type; it should be the first 3 characters
 	let courseType = "";
 	do {
-		courseType = getCourseType(substrings[index]);
+		courseType = getCourseType(substrings[index].slice(0,3));
 		index += 1;
-		// If we go out of bounds, this is a failure
-		if (substrings.length <= index) {
+		// If we go out of bounds, this may be a failure
+		if (substrings.length < index) {
 			console.log("Parser FAILED! Couldn't find course type in:", substrings);
 			return null;
 		}
 	} while ("" == courseType);
+	// In some cases, the course type and description are not separated by |
+	// This means the course type is the last substring.
+	// Fix this by splitting it, assuming all course types are 3 characters long.
+	if ((substrings.length == index) && (3 < substrings[index-1].length)) {
+		substrings.push(substrings[index-1].slice(3).trim());
+		// console.log("Fixed un-split courseType and description", substrings);
+	}
 
 	/* Assumption:
 	 * The section directly after course type contains the scheduling information.
@@ -87,9 +111,9 @@ function parseDescriptionString(descriptionString) {
 	let dataString = substrings[index];
 	let enrolled=0;
 	// Retrieve the enrolled count, which may be 0 but cannot be null or undefined.
-	[enrolled, _, newIndex] = getMatch(dataString, /Students enrolled: (\d+) (\/ \d+)?/, 0);
+	[enrolled, _, newIndex] = getMatch(dataString, /Students\senrolled:\s(\d+)\s(\/ \d+)?/, 0);
+	
 	dataString = dataString.slice(newIndex);
-	// console.log("RESULT", enrolled, "\nremaining", dataString);
 
 	// Retrieve multiple meetings from the remainder of the string.
 	let meetings = [];
@@ -101,9 +125,9 @@ function parseDescriptionString(descriptionString) {
 		}
 	} while (meeting != null);
 	// console.log("Enrolled", enrolled, "Meetings", meetings);
-	// Something is wrong, we didn't find any meetings at all. Crash.
+	// Something may be wrong, we didn't find any meetings at all.
 	if ([] == meetings) {
-		console.log("\nWARNING: NO schedule found in:", descriptionString);
+		console.log("\nWARNING: No schedule found in:", descriptionString);
 	}
 
 	// Return the sections (remember we haven't got the location in the meetings yet)
@@ -122,7 +146,7 @@ function parseScheduleString(dataString) {
 	let startEndDate="";
 	
 	// Retrieve the quarter start/end date
-	[startEndDate, _, newIndex] = getMatch(dataString, /(\d\d\/\d\d\/\d{4} - \d\d\/\d\d\/\d{4})/, "");
+	[startEndDate, _, newIndex] = getMatch(dataString, /(\d\d\/\d\d\/\d{4}\s-\s\d\d\/\d\d\/\d{4})/, "");
 	dataString = dataString.slice(newIndex);
 
 	/* We want to find the times first. This is to determine where the days are
@@ -248,10 +272,10 @@ function parseTime(timeString) {
 	if (null == timeString) {
 		return null;
 	}
-	// The first group is the hours, the second is minutes, the third is AM or PM
+	// The first group is the hours, the second is minutes, the third is A or P
 	result = timeString.match(/(\d{1,2}):(\d{2})\s?(A|P)M/);
 	if ((null != result) && (4 == result.length)) {
-		return parseInt(result[1])*60 + parseInt(result[2]) + ("PM" == result[3] ? 12*60 : 0);
+		return parseInt(result[1])*60 + parseInt(result[2]) + (("P" === result[3]) ? 12*60 : 0);
 	}
 	else {
 		return null; // Failed to find it
@@ -263,18 +287,22 @@ function testParser() {
 		"CEE 107A | 3-5 units | UG Reqs: GER:DB-EngrAppSci, WAY-SI | Class # 7814 | Section 01 | Grading: Letter or Credit/No Credit | LEC | \
 		Students enrolled: 23 09/23/2019 - 12/06/2019 Mon, Wed, Fri 1:30 PM - 2:50 PM at McCullough 115",
 		"AA 200 | 3 units | Class # 8010 | Section 01 | Grading: Letter (ABCD/NP) | LEC | Students enrolled: 68 / 90 \
-		01/06/2020 - 03/13/2020 Tue, Thu 4:30 PM - 5:50 PM at Gates B3 with Alonso, J. (PI) \
+		01/06/2020 - 03/13/2020 Tue, Thu 4:30 PM - 5:50 PM at Gates B3 with ...\
 		Instructors: Alonso, J. (PI)",
 		"AA 229 | 3-4 units | Class # 19492 | Section 01 | Grading: Letter or Credit/No Credit | LEC | \
-		Students enrolled: 15 01/06/2020 - 03/13/2020 Mon, Wed 1:30 PM - 2:50 PM at McMurtry Building 102, Oshman with Kochenderfer, M. (PI)",
+		Students enrolled: 15 01/06/2020 - 03/13/2020 Mon, Wed 1:30 PM - 2:50 PM at McMurtry Building 102, Oshman with ...",
 		// A test case with an exam date listing
-		"CEE 107A | 3-5 units | UG Reqs: GER:DB-EngrAppSci, WAY-SI | Class # 7814 | Section 01 | Grading: Letter or Credit/No Credit | LEC | Students enrolled: 23\
-		09/23/2019 - 12/06/2019 Mon, Wed, Fri 1:30 PM - 2:50 PM at McCullough 115 with Gragg, D. (PI); Stasio, K. (PI); Woodward, J. (PI)\
+		"CEE 107A | 3-5 units | UG Reqs: GER:DB-EngrAppSci, WAY-SI | Class # 7814 | Section 01 | Grading: Letter or Credit/No Credit | LEC | Students enrolled: 23 \
+		09/23/2019 - 12/06/2019 Mon, Wed, Fri 1:30 PM - 2:50 PM at McCullough 115 with ...\
 		Exam Date/Time: 2019-12-11 3:30pm - 6:30pm (Exam Schedule)",
 		// A test case with multiple meetings
-		"CEE 322 | 3 units | Class # 7893 | Section 01 | Grading: Letter or Credit/No Credit | LEC | Students enrolled: 25\
-		09/23/2019 - 12/06/2019 Mon, Wed 1:30 PM - 2:50 PM at 380-380W with Rajagopal, R. (PI)\
-		09/23/2019 - 12/06/2019 Fri 9:30 AM - 10:50 AM at McCullough 126 with Rajagopal, R. (PI)",
-		];
+		"CEE 322 | 3 units | Class # 7893 | Section 01 | Grading: Letter or Credit/No Credit | LEC |\
+		Students enrolled: 25 \
+		09/23/2019 - 12/06/2019 Mon, Wed 1:30 PM - 2:50 PM at 380-380W with ... (PI)\
+		09/23/2019 - 12/06/2019 Fri 9:30 AM - 10:50 AM at McCullough 126 with ... (PI)",
+		"CS 229 | 3-4 units | Class # 7879 | Section 01 | Grading: Letter or Credit/No Credit | LEC |\
+		Students enrolled: 375 \
+		09/23/2019 - 12/06/2019 Mon, Wed 9:30 AM - 10:50 AM at NVIDIA Auditorium with ... (PI);",
+];
 	testInputs.map( (testInput, _) => parseDescriptionString(testInput) );
 }
